c22d738b0a8a711c6b18c1112670fbee1f877721
Paul Ladd <vlendle@gmail.com>
2021-03-09 22:21:18 +0000

    Adding working async data retrieval

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed) {
+const breedDetailsFromFile = function(breed, callbackWhenFileContentsGotten) {
   console.log("breedDetailsFromFile: Calling readFile...");
   fs.readFile(`./data/${breed}.txt`, `utf8`, (error, data) => {
     console.log("In readFile callback, data grabbed");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) return data;
+    if (!error) return callbackWhenFileContentsGotten(data);
   });
 };
 
 
 // we try to get the return value
-const baby = breedDetailsFromFile('baby');
-console.log('Return Value: ', baby); // => will NOT print out details, instead we will see undefined!
+const baby = breedDetailsFromFile('baby', data => console.log(`Return value: `, data));
